4a81fd7d2541955e939ebfa358ee1d6b2beaa15a
rileyk94 <rileyk94@gmail.com>
2025-04-12 22:13:21 -0500

    feat: add player choice function

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+//create 3 variables for rock, paper, and scissors
 const rock = 1;
 const scissors = 2;
 const paper = 3;
+
+//create a function that will generate a computer Choice:
 function getComputerChoice() {
     let choice = Math.floor(Math.random()*(4-1)+1);
     if (choice === 1) {
@@ -12,5 +15,23 @@ function getComputerChoice() {
     }
 }
 
-// const test = getComputerChoice();
-// console.log(test);
+//create function to get user choice:
+function getHumanChoice() {
+    let correctInput = false;
+    while (correctInput === false) {
+        let userInput = prompt("Choose Rock, Paper or Scissors: ")
+        userInput = userInput.toLowerCase();
+        if (userInput === "rock") {
+            return rock
+            correctInput = true;
+        } else if (userInput === "scissors") {
+            return scissors;
+            correctInput = true;
+        } else if (userInput === "paper") {
+            return paper;
+            correctInput = true;
+        } else {
+            console.log("Incorrect input, please try again")
+        }
+    }
+}
